67e4784c99edbafd76d13f26606f8a1867317e88
indapp <indapp.info@gmail.com>
2017-04-09 15:14:16 +0200

    Spotify API implemented

---
+++ app/src/main/java/it/droidcon/emirror/model/AuthCode.kt
@@ -0,0 +1,10 @@
+package it.droidcon.emirror.model
+
+/**
+ * This class is part of Emirror project.
+ * Created by riccardopizzoni on 09/04/17.
+ * Copyright © 2017 INDAPP
+ * info@indapp.it
+ */
+
+data class AuthCode(val authCode: String)
---
+++ app/src/main/java/it/droidcon/emirror/model/Track.kt
@@ -0,0 +1,12 @@
+package it.droidcon.emirror.model
+
+/**
+ * This class is part of Emirror project.
+ * Created by riccardopizzoni on 09/04/17.
+ * Copyright © 2017 INDAPP
+ * info@indapp.it
+ */
+class Track (val uri: String,
+             val artist: String,
+             val image: String,
+             val title: String)
